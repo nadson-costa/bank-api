9ff075e4f4f09f365aae7f5ab26727a1433ee6ab
Nadson Costa <146302133+nadson-costa@users.noreply.github.com>
2025-12-27 18:57:26 -0300

    feat: adiciona domínio bank.nadson.dev no CORS

--- src/main/java/com/nadson/bankapi/config/SecurityConfig.java
+++ src/main/java/com/nadson/bankapi/config/SecurityConfig.java
@@ -35,7 +35,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                "http://localhost:3000",
+                "https://bank.nadson.dev"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
